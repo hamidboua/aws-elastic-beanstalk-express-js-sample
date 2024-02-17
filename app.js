f61a5577d3894cadbd8c12ffdf6ce51df57c876b
hamidboua <154917389+hamidboua@users.noreply.github.com>
2024-02-17 11:00:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! 17022024'));
+app.get('/', (req, res) => res.send('Hello World! 17022024 - update1 to aws'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
